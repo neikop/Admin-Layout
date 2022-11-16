01d87b352f09bd089c24703e3cedf5291bc5f467
Kane <monalisa@gmail.com>
2022-11-16 23:34:10 +0700

    fix endpoint

--- src/services/withdraw.ts
+++ src/services/withdraw.ts
@@ -4,7 +4,7 @@ const getWithdraw = (params?: any): Promise<WithdrawPaginateType> => client.get(
 const getDeposit = (params?: any): Promise<DepositPaginateType> => client.get(`v1/deposits`, { params });
 
 const createWithdraw = (body: any): Promise<DepositPaginateType> => client.post(`v1/withdrawals`, body);
-const updateBank = ({ playerId, body }: any): Promise<any> => client.patch(`v1/${playerId}/bank-account`, body);
+const updateBank = ({ playerId, body }: any): Promise<any> => client.patch(`v1/players/${playerId}/bank-account`, body);
 
 const transactionService = {
   updateBank,
