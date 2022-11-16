33c057f95d7c3213016629aa9df79c58ae12ee85
WindzLord <ii.khackien@gmail.com>
2022-11-16 17:20:43 +0700

    livechat

--- src/views/Chat/Chat.tsx
+++ src/views/Chat/Chat.tsx
@@ -1,9 +1,37 @@
+import { LiveChatWidget } from '@livechat/widget-react';
+import { Button } from '@mui/material';
+import { Spinner } from 'components';
+import { useState } from 'react';
+
+type VisibilityType = 'maximized' | 'minimized' | 'hidden';
+
 const Chat = () => {
+  const [isReady, setIsReady] = useState(false);
+  const [visible, setVisible] = useState<VisibilityType>('maximized');
+
   return (
     <div className='h-full flex flex-col'>
       <div className='h-[60px] flex justify-center items-center'>
         <span className='font-bold text-xl'>Trung tâm hỗ trợ</span>
       </div>
+      <Spinner className='my-20' loading={!isReady}>
+        <div className='flex justify-center'>
+          {visible === 'hidden' ? (
+            <Button variant='contained' color='secondary' onClick={() => setVisible('maximized')}>
+              Liên hệ CSKH
+            </Button>
+          ) : (
+            <></>
+          )}
+        </div>
+      </Spinner>
+
+      <LiveChatWidget
+        license={process.env.REACT_APP_LIVE_CHAT_LICENSE! ?? '14395797'}
+        visibility={visible}
+        onReady={() => setIsReady(true)}
+        onVisibilityChanged={({ visibility }) => setVisible(visibility)}
+      />
     </div>
   );
 };
