97430c813a5f37a4fca9e2c8cadc3e9fb2847c37
WindzLord <ii.khackien@gmail.com>
2022-11-21 11:12:42 +0700

    log

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
 export const API_URL = process.env.REACT_APP_API_URI!;
 export const WEB_TITLE = process.env.REACT_APP_SITE_TITLE!;
 export const LIVE_CHAT_LICENSE = process.env.REACT_APP_LIVE_CHAT_LICENSE!;
+
+console.log({ WEB_TITLE, LIVE_CHAT_LICENSE });
