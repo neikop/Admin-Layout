e0af56c5131fab7e17e0a89430d56ac5eb3e0ca7
WindzLord <ii.khackien@gmail.com>
2022-11-21 10:18:45 +0700

    Minor

--- src/views/Transaction/components/TabDeposit.tsx
+++ src/views/Transaction/components/TabDeposit.tsx
@@ -44,7 +44,7 @@ const TabDeposit = () => {
         className='bottom-[100px] left-0 right-0 pt-2'
         style={{
           backgroundColor: '#F5F5F5',
-          position: isLong ? 'fixed' : 'relative',
+          position: isLong ? 'fixed' : 'static',
           padding: isLong ? 8 : 0,
         }}
       >
