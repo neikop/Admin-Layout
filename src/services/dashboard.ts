1b68b46cf7e9dfb33d3992830885ae26f4287fc9
Kane <monalisa@gmail.com>
2022-12-09 22:56:12 +0700

    add api update

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -2,9 +2,11 @@ import { client } from './axios';
 
 const fetchBanks = (params: ParamsType): Promise<BankPaginateType> => client.get(`/api/bank`, { params });
 const createBank = (body: BankCreateType): Promise<BankType> => client.post(`/api/bank`, body);
+const updateBank = (id: string, body: BankCreateType): Promise<BankType> => client.put(`/api/bank/${id}`, body);
 
 const dashboardService = {
   fetchBanks,
   createBank,
+  updateBank,
 };
 export default dashboardService;
